30c4446ef82ee560311d833312e387949c02a44a
kowsalya <137433937+KowsyKC@users.noreply.github.com>
2024-01-31 14:20:13 +0530

    Add files via upload
    Making SQL easy with Sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'student',
-    password: 'nodecomplete'
-});
-
-module.exports = pool.promise();
+const sequelize = new Sequelize('test','root','1234', {
+  dialect:'mysql',
+  host: 'localhost'
+})
+ 
+module.exports = sequelize;
